f719fcf96ee654f47d9d2736effa1269b9ae8686
Suresh Kumar <suresh@prananvi.com>
2025-08-26 19:43:02 -0400

    standlone build and exposed API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  trailingSlash: true,
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    PORT: process.env.PORT || '0', 
+    HOSTNAME: process.env.HOSTNAME || '0.0.0.0'
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
